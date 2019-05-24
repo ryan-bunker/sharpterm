3d8e63b1e30581703a1080e97332fbc781ed651a
Ryan Bunker <ryan.bunker@gmail.com>
2019-05-24 00:40:21 -0500

    Advanced prototype that can front a bash shell

--- sharpterm/Program.cs
+++ sharpterm/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Win32.SafeHandles;
@@ -14,12 +15,6 @@ namespace sharpterm
         
         [DllImport("libutil")]
         private static extern int forkpty(out int amaster, string name, IntPtr termp, IntPtr winp);
-
-        [DllImport("libc", SetLastError = true)]
-        private static extern int read(int fd, byte[] buffer, int size);
-
-        [DllImport("libc", SetLastError = true)]
-        private static extern int write(int fd, byte[] buffer, int size);
 
         [DllImport("libc")]
         private static extern int tcgetattr(int fd, IntPtr termios_p);
@@ -35,49 +30,63 @@ namespace sharpterm
 
         static void Main(string[] args)
         {
-            var termSettings = Marshal.AllocHGlobal(4 * 4 + 4 * 20);
-            cfmakeraw(termSettings);
-            
-            var pid = forkpty(out int masterFd, null, termSettings, IntPtr.Zero);
+            var pid = forkpty(out int masterFd, null, IntPtr.Zero, IntPtr.Zero);
             if (0 == pid)
             {
                 // this is the child
-                var stdinReader = new StreamReader(Console.OpenStandardInput());
-
-                while (true)
-                {
-                    var line = stdinReader.ReadLine();
-                    if (line == "quit") return;
-                    Console.WriteLine(line.Reverse().ToArray());
-                }
+                execv("/bin/sh", new[] {"/bin/sh", null});
             }
             else
             {
+                var termSettings = Marshal.AllocHGlobal(4 * 4 + 4 * 20);
+                tcgetattr(masterFd, termSettings);
+                cfmakeraw(termSettings);
+                tcsetattr(masterFd, TCSANOW, termSettings);
+
                 var masterFs = new FileStream(new SafeFileHandle(new IntPtr(masterFd), true), FileAccess.Read);
-                var masterReader = new StreamReader(masterFs);
                 var masterFsW = new FileStream(new SafeFileHandle(new IntPtr(masterFd), true), FileAccess.Write);
-                var masterWriter = new StreamWriter(masterFsW);
                 
                 var cts = new CancellationTokenSource();
-                var readTask = ReadStreamAsync(pid, masterReader, cts.Token);
+                var readTask = ReadStreamAsync(masterFs, cts.Token);
 
+                var buffer = new MemoryStream(4096);
                 while (true)
                 {
-                    var msg = Console.ReadLine();
-                    masterWriter.WriteLine(msg);
-                    masterWriter.Flush();
-                    if (msg == "quit") return;
+                    var key = Console.ReadKey(true);
+                    if (key.Key == ConsoleKey.Backspace)
+                    {
+                        Console.CursorLeft--;
+                        Console.Write(' ');
+                        Console.CursorLeft--;
+                        buffer.Position--;
+                    }
+                    else if (key.Key == ConsoleKey.Enter)
+                    {
+                        Console.WriteLine();
+                        buffer.WriteByte((byte)'\n');
+                        
+                        masterFsW.Write(buffer.GetBuffer(), 0, (int)buffer.Length);
+                        masterFsW.Flush();
+                        buffer.Position = 0;
+                        buffer.SetLength(0);
+                    }
+                    else if (key.KeyChar >= ' ' && key.KeyChar <= (char) 127)
+                    {
+                        Console.Write(key.KeyChar);
+                        buffer.WriteByte((byte)key.KeyChar);
+                    }
                 }
             }
         }
 
-        static async Task ReadStreamAsync(int childPid, TextReader reader, CancellationToken ct)
+        static async Task ReadStreamAsync(Stream stream, CancellationToken ct)
         {
+            var buffer = new byte[128];
             while (!ct.IsCancellationRequested)
             {
-                var line = await reader.ReadLineAsync();
-                if (line == null) return;
-                Console.WriteLine($"[{childPid}]: {line}");
+                int read = await stream.ReadAsync(buffer, ct);
+                var line = Encoding.ASCII.GetString(buffer, 0, read);
+                await Console.Out.WriteAsync(line);
             }
         }
     }
