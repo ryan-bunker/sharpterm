614782a39189eeaadeddeac2f1db2ddb4a9e5bb2
Ryan Bunker <ryan.bunker@gmail.com>
2019-05-31 11:31:16 -0500

    Prototype of text rendering

--- sharpterm/Program.cs
+++ sharpterm/Program.cs
@@ -1,209 +1,169 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.IO;
+using System.Net;
 using System.Numerics;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using ImGuiNET;
-using Microsoft.Win32.SafeHandles;
 using Veldrid;
 using Veldrid.Sdl2;
-using Veldrid.SPIRV;
 using Veldrid.StartupUtilities;
+using Veldrid.SPIRV;
+using SharpFont;
+using Encoding = System.Text.Encoding;
 
-namespace sharpterm
+namespace GettingStarted
 {
-    internal class Program
+    class Program
     {
-        private const int TCSANOW = 1;
-        
-        [DllImport("libutil")]
-        private static extern int forkpty(out int amaster, string name, IntPtr termp, IntPtr winp);
-
-        [DllImport("libc")]
-        private static extern int tcgetattr(int fd, IntPtr termios_p);
-
-        [DllImport("libc")]
-        private static extern int tcsetattr(int fd, int optional_actions, IntPtr termios_p);
-        
-        [DllImport("libc")]
-        private static extern void cfmakeraw(IntPtr termios_p);
-
-        [DllImport("libc")]
-        private static extern int execv(string pathname, string[] argv);
-
         private static GraphicsDevice _graphicsDevice;
         private static CommandList _commandList;
         private static DeviceBuffer _vertexBuffer;
         private static DeviceBuffer _indexBuffer;
         private static Shader[] _shaders;
         private static Pipeline _pipeline;
+        private static ResourceSet _textureSet;
 
         private const string VertexCode = @"
 #version 450
+
 layout(location = 0) in vec2 Position;
-layout(location = 1) in vec4 Color;
-layout(location = 0) out vec4 fsin_Color;
+layout(location = 1) in vec2 Texture;
+layout(location = 2) in vec4 Color;
+
+layout(location = 0) out vec2 fsin_TexCoords;
+
 void main()
 {
     gl_Position = vec4(Position, 0, 1);
-    fsin_Color = Color;
+    fsin_TexCoords = Texture;
 }";
 
         private const string FragmentCode = @"
 #version 450
-layout(location = 0) in vec4 fsin_Color;
+
+layout(location = 0) in vec2 fsin_TexCoords;
 layout(location = 0) out vec4 fsout_Color;
+
+layout(set = 1, binding = 1) uniform texture2D SurfaceTexture;
+layout(set = 1, binding = 2) uniform sampler SurfaceSampler;
+
 void main()
 {
-    fsout_Color = fsin_Color;
+    float c = texture(sampler2D(SurfaceTexture, SurfaceSampler), fsin_TexCoords).r;
+    fsout_Color = vec4(c, c, c, 1);
 }";
-        
+
         static void Main(string[] args)
         {
-            VeldridStartup.CreateWindowAndGraphicsDevice(
-                new WindowCreateInfo(50, 50, 1920, 1080, WindowState.Normal, "ImGui Test"),
-                out var window,
-                out var gd);
-
-            var cl = gd.ResourceFactory.CreateCommandList();
-
-            // [1]
-            var imguiRenderer = new ImGuiRenderer(
-                gd,
-                gd.MainSwapchain.Framebuffer.OutputDescription,
-                window.Width,
-                window.Height);
-
-            var io = ImGui.GetIO();
-//
-//            var data = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(ushort)) * 3);
-//            var ranges = new ImVector<ushort>(3, 3, data);
-//            ranges[0] = 0xf000;
-//            ranges[1] = 0xf3ff;
-//            ranges[2] = 0;
-//            
-            var font = io.Fonts.AddFontFromFileTTF("Inconsolata-Regular.ttf", 16);
-//            io.Fonts.Build();
-            imguiRenderer.RecreateFontDeviceTexture();
-            if (!font.IsLoaded())
-                throw new Exception("Could not load font");
-
-            var field = typeof(ImGuiRenderer).GetField("_scaleFactor", BindingFlags.Instance | BindingFlags.NonPublic);
-            field.SetValue(imguiRenderer, new Vector2(2.5f, 2.5f));
-
-            window.Resized += () =>
+            WindowCreateInfo windowCI = new WindowCreateInfo()
             {
-                imguiRenderer.WindowResized(window.Width, window.Height);
-                gd.MainSwapchain.Resize((uint)window.Width, (uint)window.Height);
+                X = 100,
+                Y = 100,
+                WindowWidth = 1920,
+                WindowHeight = 1920,
+                WindowTitle = "Veldrid Tutorial"
             };
-            
-            var buffer = new StringBuilder("[ryan@manjaro-xfce-vm-laptop ~]$ ");
-//            for (int i = 127; i < 256 + 128; i++)
-//                buffer.AppendLine($"{i}: {(char)i}");
+            Sdl2Window window = VeldridStartup.CreateWindow(ref windowCI);
 
-            var nextToggle = DateTime.UtcNow.AddMilliseconds(750);
-            bool visible = true;
+            _graphicsDevice = VeldridStartup.CreateGraphicsDevice(window);
+
+            CreateResources();
+
             while (window.Exists)
             {
-                var snapshot = new InputSnapshotWrapper(window.PumpEvents(), new Vector2(2.5f, 2.5f));
-                imguiRenderer.Update(1f / 60f, snapshot); // [2]
+                window.PumpEvents();
 
-                bool scrollToBottom = false;
-                foreach (var c in snapshot.KeyCharPresses)
+                if (window.Exists)
                 {
-                    buffer.Append(c);
-                    scrollToBottom = true;
+                    Draw();
                 }
-
-                foreach (var k in snapshot.KeyEvents)
-                    if (k.Down && k.Key == Key.Enter)
-                    {
-                        buffer.AppendLine();
-                        scrollToBottom = true;
-                    }
-
-                if (DateTime.UtcNow >= nextToggle)
-                {
-                    visible = !visible;
-                    nextToggle = nextToggle.AddMilliseconds(750);
-                }
-
-                // Draw whatever you want here.
-                ImGui.Begin("Test Window", ImGuiWindowFlags.NoBackground | ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoNav);
-                ImGui.SetWindowPos(new Vector2(0, 0));
-                ImGui.SetWindowSize(new Vector2(window.Width / 2.5f, window.Height / 2.5f));
-                {
-                    ImGui.PushFont(font);
-                    
-                    ImGui.TextWrapped(buffer.ToString() + (visible ? '_' : ' '));
-                    if (scrollToBottom)
-                        ImGui.SetScrollHereY(1);
-                }
-
-                cl.Begin();
-                cl.SetFramebuffer(gd.MainSwapchain.Framebuffer);
-                cl.ClearColorTarget(0, new RgbaFloat(0, 0, 0.2f, 1f));
-                imguiRenderer.Render(gd, cl); // [3]
-                cl.End();
-                gd.SubmitCommands(cl);
-                gd.SwapBuffers(gd.MainSwapchain);
-            }       
-        }
-
-        private class InputSnapshotWrapper : InputSnapshot
-        {
-            private InputSnapshot _internal;
-            private Vector2 _scale;
-
-            public InputSnapshotWrapper(InputSnapshot @internal, Vector2 scale)
-            {
-                _internal = @internal;
-                _scale = scale;
             }
 
-            public bool IsMouseDown(MouseButton button) => _internal.IsMouseDown(button);
-
-            public IReadOnlyList<KeyEvent> KeyEvents => _internal.KeyEvents;
-
-            public IReadOnlyList<MouseEvent> MouseEvents => _internal.MouseEvents;
-
-            public IReadOnlyList<char> KeyCharPresses => _internal.KeyCharPresses;
-
-            public Vector2 MousePosition => _internal.MousePosition / _scale;
-
-            public float WheelDelta => _internal.WheelDelta;
+            DisposeResources();
         }
+
+        private class CharCell
+        {
+            public float X { get; set; }
+            public float Y { get; set; }
+            public float W { get; set; }
+            public float H { get; set; }
+            public float R => X + W;
+            public float B => Y + H;
+            public float Aspect => W / H;
+        }
+
+        private static CharCell[] Cells = new CharCell[256];
 
         private static void CreateResources()
         {
             ResourceFactory factory = _graphicsDevice.ResourceFactory;
+            
+            var lib = new Library();
+            var face = new Face(lib, @"./Inconsolata-Regular.ttf");
+            face.SetPixelSizes(96, 0);
+            uint font_width = face.Size.Metrics.NominalWidth;
+            uint font_height = (uint)face.Size.Metrics.Height.ToInt32();
 
+            var surfaceTexture = factory.CreateTexture(new TextureDescription(
+                16 * font_width, 16 * font_height, 1, 1, 1,
+                PixelFormat.R8_UNorm, TextureUsage.Sampled, TextureType.Texture2D));
+
+            uint x = 0, y = 0;
+            for (uint ci = 0; ci < 256; ci++)
+            {
+                var c = (char)ci;
+                if (ci % 16 == 0) {
+                    x = 0;
+                    y = font_height * (ci / 16);
+                }
+                face.LoadChar((uint)Char.ConvertToUtf32(c.ToString(), 0), LoadFlags.Render, LoadTarget.Normal);
+                var bmpIn = face.Glyph.Bitmap;
+	
+                uint xpos = x + (uint)face.Glyph.BitmapLeft;
+                uint ypos = y - (uint)face.Glyph.BitmapTop + (uint)face.Size.Metrics.Ascender.ToInt32();
+                
+                //g.DrawRectangle(Pens.Red, x, y, face.Glyph.Advance.X.ToInt32(), font_height);
+                Cells[ci] = new CharCell
+                {
+                    X = x / (16f * font_width),
+                    Y = y / (16f * font_height),
+                    W = face.Glyph.Advance.X.ToSingle() / (16f * font_width),
+                    H = 1f / 16f
+                };
+
+                _graphicsDevice.UpdateTexture(surfaceTexture, bmpIn.Buffer, (uint)(bmpIn.Rows * bmpIn.Width),
+                    xpos, ypos, 0, (uint)bmpIn.Width, (uint)bmpIn.Rows, 1, 0, 0);
+
+                x += font_width;
+            }
+            
+            var surfaceTextureView = factory.CreateTextureView(surfaceTexture);
+
+            var cell = Cells['&'];
+            var height = 96f / 1920f;
+            var width = cell.Aspect * height;
             VertexPositionColor[] quadVertices =
             {
-                new VertexPositionColor(new Vector2(-.75f, .75f), RgbaFloat.Red),
-                new VertexPositionColor(new Vector2(.75f, .75f), RgbaFloat.Green),
-                new VertexPositionColor(new Vector2(-.75f, -.75f), RgbaFloat.Blue),
-                new VertexPositionColor(new Vector2(.75f, -.75f), RgbaFloat.Yellow)
+                new VertexPositionColor(new Vector2(-width / 2f, height / 2f), new Vector2(cell.X, cell.Y),  RgbaFloat.Red),
+                new VertexPositionColor(new Vector2(width / 2f, height / 2f), new Vector2(cell.R, cell.Y), RgbaFloat.Green),
+                new VertexPositionColor(new Vector2(-width / 2f, -height / 2f), new Vector2(cell.X, cell.B), RgbaFloat.Blue),
+                new VertexPositionColor(new Vector2(width / 2f, -height / 2f), new Vector2(cell.R, cell.B), RgbaFloat.Yellow)
             };
             BufferDescription vbDescription = new BufferDescription(
-                4 * VertexPositionColor.SizeInBytes,
+                (uint)quadVertices.Length * VertexPositionColor.SizeInBytes,
                 BufferUsage.VertexBuffer);
             _vertexBuffer = factory.CreateBuffer(vbDescription);
             _graphicsDevice.UpdateBuffer(_vertexBuffer, 0, quadVertices);
 
             ushort[] quadIndices = { 0, 1, 2, 3 };
             BufferDescription ibDescription = new BufferDescription(
-                4 * sizeof(ushort),
+                (uint)quadIndices.Length * sizeof(ushort),
                 BufferUsage.IndexBuffer);
             _indexBuffer = factory.CreateBuffer(ibDescription);
             _graphicsDevice.UpdateBuffer(_indexBuffer, 0, quadIndices);
-
+            
             VertexLayoutDescription vertexLayout = new VertexLayoutDescription(
                 new VertexElementDescription("Position", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float2),
+                new VertexElementDescription("Texture", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float2),
                 new VertexElementDescription("Color", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float4));
 
             ShaderDescription vertexShaderDesc = new ShaderDescription(
@@ -216,6 +176,11 @@ void main()
                 "main");
 
             _shaders = factory.CreateFromSpirv(vertexShaderDesc, fragmentShaderDesc);
+
+            var textureLayout = factory.CreateResourceLayout(new ResourceLayoutDescription(
+                new ResourceLayoutElementDescription("SurfaceTexture", ResourceKind.TextureReadOnly,
+                    ShaderStages.Fragment),
+                new ResourceLayoutElementDescription("SurfaceSampler", ResourceKind.Sampler, ShaderStages.Fragment)));
 
             // Create pipeline
             GraphicsPipelineDescription pipelineDescription = new GraphicsPipelineDescription();
@@ -231,13 +196,29 @@ void main()
                 depthClipEnabled: true,
                 scissorTestEnabled: false);
             pipelineDescription.PrimitiveTopology = PrimitiveTopology.TriangleStrip;
-            pipelineDescription.ResourceLayouts = System.Array.Empty<ResourceLayout>();
+            pipelineDescription.ResourceLayouts = new[] {textureLayout};
             pipelineDescription.ShaderSet = new ShaderSetDescription(
-                vertexLayouts: new VertexLayoutDescription[] { vertexLayout },
+                vertexLayouts: new[] { vertexLayout },
                 shaders: _shaders);
             pipelineDescription.Outputs = _graphicsDevice.SwapchainFramebuffer.OutputDescription;
+            
 
             _pipeline = factory.CreateGraphicsPipeline(pipelineDescription);
+
+            _textureSet = factory.CreateResourceSet(new ResourceSetDescription(
+                textureLayout,
+                surfaceTextureView,
+                factory.CreateSampler(new SamplerDescription
+                {
+                    AddressModeU = SamplerAddressMode.Clamp,
+                    AddressModeV = SamplerAddressMode.Clamp,
+                    AddressModeW = SamplerAddressMode.Clamp,
+                    Filter = SamplerFilter.Anisotropic,
+                    LodBias = 0,
+                    MinimumLod = 0,
+                    MaximumLod = uint.MaxValue,
+                    MaximumAnisotropy = 4
+                })));
 
             _commandList = factory.CreateCommandList();
         }
@@ -249,15 +230,16 @@ void main()
 
             // We want to render directly to the output window.
             _commandList.SetFramebuffer(_graphicsDevice.SwapchainFramebuffer);
-            _commandList.ClearColorTarget(0, RgbaFloat.Black);
+            _commandList.ClearColorTarget(0, RgbaFloat.Clear);
 
             // Set all relevant state to draw our quad.
             _commandList.SetVertexBuffer(0, _vertexBuffer);
             _commandList.SetIndexBuffer(_indexBuffer, IndexFormat.UInt16);
             _commandList.SetPipeline(_pipeline);
+            _commandList.SetGraphicsResourceSet(0, _textureSet);
             // Issue a Draw command for a single instance with 4 indices.
             _commandList.DrawIndexed(
-                indexCount: 4,
+                indexCount: _indexBuffer.SizeInBytes / sizeof(ushort),
                 instanceCount: 1,
                 indexStart: 0,
                 vertexOffset: 0,
@@ -283,80 +265,19 @@ void main()
             _indexBuffer.Dispose();
             _graphicsDevice.Dispose();
         }
-
-        static void MainPty(string[] args)
-        {
-            var pid = forkpty(out int masterFd, null, IntPtr.Zero, IntPtr.Zero);
-            if (0 == pid)
-            {
-                // this is the child
-                execv("/bin/sh", new[] {"/bin/sh", null});
-            }
-            else
-            {
-                var termSettings = Marshal.AllocHGlobal(4 * 4 + 4 * 20);
-                tcgetattr(masterFd, termSettings);
-                cfmakeraw(termSettings);
-                tcsetattr(masterFd, TCSANOW, termSettings);
-
-                var masterFs = new FileStream(new SafeFileHandle(new IntPtr(masterFd), true), FileAccess.Read);
-                var masterFsW = new FileStream(new SafeFileHandle(new IntPtr(masterFd), true), FileAccess.Write);
-                
-                var cts = new CancellationTokenSource();
-                var readTask = ReadStreamAsync(masterFs, cts.Token);
-
-                var buffer = new MemoryStream(4096);
-                while (true)
-                {
-                    var key = Console.ReadKey(true);
-                    if (key.Key == ConsoleKey.Backspace)
-                    {
-                        Console.CursorLeft--;
-                        Console.Write(' ');
-                        Console.CursorLeft--;
-                        buffer.Position--;
-                    }
-                    else if (key.Key == ConsoleKey.Enter)
-                    {
-                        Console.WriteLine();
-                        buffer.WriteByte((byte)'\n');
-                        
-                        masterFsW.Write(buffer.GetBuffer(), 0, (int)buffer.Length);
-                        masterFsW.Flush();
-                        buffer.Position = 0;
-                        buffer.SetLength(0);
-                    }
-                    else if (key.KeyChar >= ' ' && key.KeyChar <= (char) 127)
-                    {
-                        Console.Write(key.KeyChar);
-                        buffer.WriteByte((byte)key.KeyChar);
-                    }
-                }
-            }
-        }
-
-        static async Task ReadStreamAsync(Stream stream, CancellationToken ct)
-        {
-            var buffer = new byte[128];
-            while (!ct.IsCancellationRequested)
-            {
-                int read = await stream.ReadAsync(buffer, ct);
-                var line = Encoding.ASCII.GetString(buffer, 0, read);
-                await Console.Out.WriteAsync(line);
-            }
-        }
     }
 
     struct VertexPositionColor
     {
-        public const uint SizeInBytes = 24;
+        public const uint SizeInBytes = 32;
         public Vector2 Position;
+        public Vector2 Texture;
         public RgbaFloat Color;
-        public VertexPositionColor(Vector2 position, RgbaFloat color)
+        public VertexPositionColor(Vector2 position, Vector2 texture, RgbaFloat color)
         {
+            Texture = texture;
             Position = position;
             Color = color;
         }
     }
-
 }
